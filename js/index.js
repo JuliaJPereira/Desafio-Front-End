6c6f5e60653636fba4f87261b6b5beb396c4ee0e
Julia Januario Pereira <juliapereira.odc@gmail.com>
2021-04-23 14:38:24 -0300

    Paginação da API de produtos

--- js/index.js
+++ js/index.js
@@ -1,14 +1,12 @@
-axios
-  .get(
-    "https://frontend-intern-challenge-api.iurykrieger.vercel.app/products?page=1"
-  )
-  .then((response) => {
-    console.log(response.data);
-    console.log(response.data.products);
+let url =
+  "https://frontend-intern-challenge-api.iurykrieger.vercel.app/products?page=1";
+
+function buscaProdutos() {
+  axios.get(url).then((response) => {
+    url = `https://${response.data.nextPage}`
     let containerProdutos = document.getElementById("container-itens-produtos");
 
     response.data.products.forEach((produto) => {
-        console.log(produto)
       containerProdutos.innerHTML += `
             <div class="produto">
                 <img
@@ -19,11 +17,25 @@ axios
                 <p class="descricao">
                     ${produto.description}
                 </p>
-                <p class="de">De: ${produto.oldPrice.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'})}</p>
-                <p class="por">Por: ${produto.price.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'})}</p>
-                <p class="parcelado">ou ${produto.installments.count}x de ${produto.installments.value.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'})}</p>
+                <p class="de">De: ${produto.oldPrice.toLocaleString("pt-BR", {
+                  style: "currency",
+                  currency: "BRL",
+                })}</p>
+                <p class="por">Por: ${produto.price.toLocaleString("pt-BR", {
+                  style: "currency",
+                  currency: "BRL",
+                })}</p>
+                <p class="parcelado">ou ${
+                  produto.installments.count
+                }x de ${produto.installments.value.toLocaleString("pt-BR", {
+        style: "currency",
+        currency: "BRL",
+      })}</p>
                 <button class="botao-comprar-produto">Comprar</button>
             </div>
         `;
     });
   });
+}
+
+buscaProdutos();
